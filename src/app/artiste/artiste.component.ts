df10c2558de25f4f1029f1a1e30dbb128e619fe4
Tristou <tristancha87@gmail.com>
2023-04-10 00:51:45 +0200

    add artists in html

--- src/app/artiste/artiste.component.ts
+++ src/app/artiste/artiste.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ArtistService} from "../services/artist/artist.service";
+import {DomSanitizer} from "@angular/platform-browser";
+import {Artist} from "../Model/artiste";
 
 @Component({
   selector: 'app-artiste',
@@ -6,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./artiste.component.css']
 })
 export class ArtisteComponent {
-
+  public artists : Array<Artist>
+  constructor(public songService: ArtistService, public sanitizer: DomSanitizer) {
+    this.artists = this.songService.artists
+  }
 }
