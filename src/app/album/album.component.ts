98076b4e94eda269b1e5a1c6241ec880a1cd0ec9
Tristou <tristancha87@gmail.com>
2023-04-10 01:01:41 +0200

    add albums in html

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {AlbumService} from "../services/album/album.service";
+import {Album} from "../Model/album";
+
+
 
 @Component({
   selector: 'app-album',
@@ -6,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent {
-
+  public albums : Array<Album>
+  constructor(public songService: AlbumService,) {
+    this.albums = this.songService.albums;
+  }
 }
