c04dd137196e670eadbf7f5789f62e2edc700597
Tristou <tristancha87@gmail.com>
2023-04-10 20:46:42 +0200

    search mise en place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MusiqueComponent } from './musique/musique.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { AlbumComponent } from './album/album.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AlbumComponent } from './album/album.component';
     MusiqueComponent,
     PlaylistComponent,
     AccueilComponent,
-    AlbumComponent
+    AlbumComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/search/search.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private BASE_URL: string = "https://mmi.unilim.fr/~morap01/L250/public/index.php"
+  private _search: Array<any> = []
+
+
+
+}
